0e45be146a2842401a76d600a57233bc093886b6
白熱 <sonne@asaki.me>
2024-06-03 15:49:16 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import "react-mosaic-component/react-mosaic-component.css";
 import "./style.css";
-import "@univerjs/design/lib/index.css";
-import "@univerjs/sheets-formula/lib/index.css";
-import "@univerjs/sheets-numfmt/lib/index.css";
-import "@univerjs/sheets-ui/lib/index.css";
-import "@univerjs/ui/lib/index.css";
 
-import { LocaleType, LogLevel, Tools, Univer } from "@univerjs/core";
+import { LocaleType, Tools, Univer, UniverInstanceType } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
 import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
@@ -19,6 +14,7 @@ import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
 import { UniverUIPlugin } from "@univerjs/ui";
 import ReactDOM from "react-dom";
 import { Mosaic, MosaicWindow } from "react-mosaic-component";
+import { zhCN, enUS } from 'univer:locales'
 
 import { DEFAULT_WORKBOOK_DATA_DEMO } from "./data";
 
@@ -28,8 +24,11 @@ function factory(id: string) {
   return function createUniverOnContainer() {
     const univer = new Univer({
       theme: defaultTheme,
-      locale: LocaleType.ZH_CN,
-      logLevel: LogLevel.VERBOSE,
+      locale: LocaleType.EN_US,
+      locales: {
+        [LocaleType.ZH_CN]: zhCN,
+        [LocaleType.EN_US]: enUS,
+      },
     });
 
     univer.registerPlugin(UniverDocsPlugin, {
@@ -39,9 +38,6 @@ function factory(id: string) {
     univer.registerPlugin(UniverRenderEnginePlugin);
     univer.registerPlugin(UniverUIPlugin, {
       container: id,
-      header: true,
-      toolbar: true,
-      footer: true,
     });
 
     // sheets plugin
@@ -54,7 +50,7 @@ function factory(id: string) {
     univer.registerPlugin(UniverSheetsFormulaPlugin);
 
     // create univer sheet instance
-    univer.createUniverSheet(Tools.deepClone(DEFAULT_WORKBOOK_DATA_DEMO));
+    univer.createUnit(UniverInstanceType.UNIVER_SHEET, Tools.deepClone(DEFAULT_WORKBOOK_DATA_DEMO));
 
     univers.push(univer);
   };
